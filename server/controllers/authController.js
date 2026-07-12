a33058e1b9fb8833b2dffe8139b30dc21bb63e4d
LAXMINARAYAN24 <sahulucky2411@gmail.com>
2026-07-12 20:02:32 +0530

    Debug environment variables

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,12 +8,15 @@ import User from "../models/User.js";
 //   });
 // };
 const generateToken = (userId) => {
+  console.log("Inside generateToken");
   console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
   console.log("JWT_SECRET length:", process.env.JWT_SECRET?.length);
 
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 };
 
 // POST /api/auth/signup
